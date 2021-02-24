8d3b0a6835d6703ffc23906732c8fc15c8a9e5dc
oday abu saleh <adlioday@gmail.com>
2021-02-24 22:14:48 +0000

    create spot app with user authentication

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dhj0qswpc'
+  config.api_key = '258174156815164'
+  config.api_secret = '_IUtEwXYoXYemtYTNI_Ik9r8YYE'
+  config.secure = true
+  config.cdn_subdomain = true
+end
