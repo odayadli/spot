7c25fc1d2ea75a76a0e7d82736bf3e5429a13fa3
roberto silva <roberto.lns10@gmail.com>
2021-03-02 20:00:18 +0000

    updated_login

--- db/schema.rb
+++ db/schema.rb
@@ -80,5 +80,4 @@ ActiveRecord::Schema.define(version: 2021_02_26_165018) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "categories", "services"
   add_foreign_key "services", "users"
-  add_foreign_key "user_types", "users"
 end
