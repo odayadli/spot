96e7d9e55f143ee6c59a265e3197721898e161d4
oday abu saleh <adlioday@gmail.com>
2021-03-09 21:01:22 +0000

    navabr changing

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,6 +5,6 @@ class Service < ApplicationRecord
   has_many :bookings
   has_many :reviews
   has_one_attached :profile_pic
-  has_many_attached :photo
+  has_one_attached :photo
   belongs_to :trainer, class_name: 'User', foreign_key: 'user_id'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
              # controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  get  'services/filter'
+  get 'bookings/bookings_requests'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :services do
     resources :bookings, only: %i[new create]
