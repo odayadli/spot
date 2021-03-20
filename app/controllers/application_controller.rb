595eba61cf25b91663435fcd46da859fca046be3
oday abu saleh <adlioday@gmail.com>
2021-03-20 12:55:57 +0000

    fixing the footer and profile_pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name profile_pic])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name profile_pic])
+
   end
 
   def default_url_options
