cea327f500b95b194068cb3a025b8bc6ede1d38c
oday abu saleh <adlioday@gmail.com>
2021-03-23 18:49:45 +0000

    background

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get  'services/filter_price'
   get  'services/filter_distance'
   get 'bookings/bookings_requests'
+  get 'services/access_denied'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :services do
     resources :bookings, only: %i[new create]
