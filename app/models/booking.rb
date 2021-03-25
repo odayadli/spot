376722ba0fcc1bcb1366604eabd4263775e44719
oday abu saleh <adlioday@gmail.com>
2021-03-25 18:50:47 +0000

    change logo

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,12 +2,12 @@ class Booking < ApplicationRecord
   belongs_to :trainee, class_name: 'User', foreign_key: 'user_id'
   belongs_to :service
   has_many :reviews
-  validate :end_date_after_start_date
-
-  private
-  def end_date_after_start_date
-    return if end_date.blank? || start_date.blank?
-    errors.add(:end_date, 'must be after the start date') if end_date < start_date
-    errors.add(:start_date, "can't be before the date of today") if start_date < DateTime.now
-  end
+  # validate :end_date_after_start_date
+  #
+  # private
+  # def end_date_after_start_date
+  #   return if end_date.blank? || start_date.blank?
+  #   errors.add(:end_date, 'must be after the start date') if end_date < start_date
+  #   errors.add(:start_date, "can't be before the date of today") if start_date < DateTime.now
+  # end
 end
